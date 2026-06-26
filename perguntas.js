a55bbd1c8aed7520d43045fd6a121b667460f0c2
matheusmartinsoliveira07-byte <matheus.martins.oliveira07@escola.pr.gov.br>
2026-06-26 13:53:20 -0300

    ..dd

--- perguntas.js
+++ perguntas.js
@@ -5,25 +5,25 @@ criaCartao(
 )
 
 criaCartao(
-    'perguntas',
+    'robotica',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'respostas',
+    'robotica',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'robotica',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'robotica',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
